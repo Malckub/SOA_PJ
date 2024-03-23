45e8eff2133f54213981da74f8a1c50713a5e90d
Malc <mill-4456@hotmail.com>
2024-03-24 06:49:37 +0700

    up

---
+++ demo/src/main/java/com/example/demo/Controller/roomcontroller.java
@@ -0,0 +1,27 @@
+package com.example.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.Repository.RoomRepository;
+
+@Controller
+@RequestMapping("/room")
+public class roomcontroller {
+	private final RoomRepository roomRepository;
+
+	@Autowired
+	public roomcontroller(RoomRepository roomRepository) {
+		this.roomRepository = roomRepository;
+	}
+
+	@GetMapping
+	public String getAllRooms(Model model) {
+		model.addAttribute("Room",roomRepository.findAll());
+		return "room";
+	}
+
+
+}
--- demo/src/main/java/com/example/demo/Repository/RoomRepository.java
+++ demo/src/main/java/com/example/demo/Repository/RoomRepository.java
@@ -1,7 +1,10 @@
-package com.example.demo;
+package com.example.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.Model.Room;
+
 
 public interface RoomRepository extends JpaRepository<Room,Integer>{
+	
 }
