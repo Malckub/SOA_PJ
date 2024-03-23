83e8164abdf6c3961e464ff82618c8637abe1bb7
Malc <mill-4456@hotmail.com>
2024-03-24 02:49:42 +0700

    chane pj

---
+++ project/src/main/java/com/project/SOA_G2/SoaG2Application.java
@@ -0,0 +1,13 @@
+package com.project.SOA_G2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaG2Application.class, args);
+	}
+
+}
--- project/src/test/java/com/project/SOA_G2/SoaG2ApplicationTests.java
+++ project/src/test/java/com/project/SOA_G2/SoaG2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.project.project;
+package com.project.SOA_G2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectApplicationTests {
+class SoaG2ApplicationTests {
 
 	@Test
 	void contextLoads() {
